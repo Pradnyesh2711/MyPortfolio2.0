71b571a31295d17573f0d02b0367c847fa5d7f61
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-07-05 12:40:05 +0530

    Done and Dusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
             
           </p>
           <div>
-          <img className="object-right rounded-full h-60" src={"src/components/IMG_20230113_092506~2-min.jpg"} alt="Logo" />
+          <img className="object-right rounded-full h-60" src={"src/components/IMG202301130925062min.jpg"} alt="Logo" />
           </div>
         </div>
       </div>
